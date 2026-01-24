27ea902fbc28df27d069515b8e0c6b96c46596d8
Fili258-oss <marinorubianobotina@gmail.com>
2026-01-24 12:06:35 -0500

    Handle error

--- example.com/hello/hello.go
+++ example.com/hello/hello.go
@@ -2,11 +2,29 @@ package main
 
 import (
     "fmt"
-	"example.com/greetings"    
+	"example.com/greetings"
+	"log"    
 )
 
 func main() {
+
+	// Set properties of the predefined Logger, including
+    // the log entry prefix and a flag to disable printing
+    // the time, source file, and line number.
+    log.SetPrefix("greetings: ")
+    log.SetFlags(0)
+
+	message, err := greetings.Hello("Marino Botina")
+
+	// If an error was returned, print it to the console and
+    // exit the program.
+    if err != nil {
+        log.Fatal(err)
+    }
+
     // Get a greeting message and print it.
-    message := greetings.Hello("Marino")
+    //message := greetings.Hello("Marino")
+	// If no error was returned, print the returned message
+    // to the console.
     fmt.Println(message)
 }
